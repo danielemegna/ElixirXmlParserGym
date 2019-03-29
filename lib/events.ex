ac47ff3afce4a9c24e26859379855f5f205b12bc
Daniele Megna <megna.dany@gmail.com>
2019-03-29 15:21:32 +0100

    Update events parse and extraction examples

--- lib/events.ex
+++ lib/events.ex
@@ -16,25 +16,41 @@ defmodule Events do
           payload: ~x"./COLUMN[@NAME='PAYLOAD']/text()"s
       )end)
       |> Stream.map(fn(event) ->
+        emitted_at = event.emitted_at # Regex.named_captures(~r/(?<day>[0-9]{2})-(?<month>[A-Z]{3})-(?<year>[0-9]{2})/, event.emitted_at)
         payload = Poison.decode!(event.payload)
-        %{ event | payload: payload } |> Map.put(:contract, payload["contract"]["raw"])
+
+        event
+          |> Map.put(:payload, payload)
+          |> Map.put(:emitted_at, emitted_at)
+          |> Map.put(:contract, payload["contract"]["raw"])
       end)
 
     results = events 
+      #|> Stream.filter(&(&1.aggregate_id == "a0w0N00000AWjZFQA1"))
+      #|> Stream.filter(&(&1.type == "SetupReceivedEvent"))
       |> Stream.filter(& !is_nil(&1.contract))
       |> Stream.filter(& !is_nil(&1.contract["salesPoints"]))
-      |> Stream.filter(&(&1.aggregate_id == "a0w0N00000AWjZFQA1"))
-      #|> Stream.filter(&(&1.type == "SetupReceivedEvent"))
-      |> Enum.take(10)
+      #|> Stream.map(& [
+      #    &1.aggregate_id,
+      #    &1.emitted_at,
+      #    &1.type,
+      #    &1.contract["merchant"]["ONLUSType"]
+      #  ]
+      #)
+      |> Stream.flat_map(& &1.contract["salesPoints"])
+      |> Stream.flat_map(& &1["acquiring"]["schemes"])
+      |> Stream.flat_map(& &1["properties"])
+      |> Stream.uniq
+      |> Enum.take(5)
+
       
     IO.inspect results
-      |> Enum.map(fn(e) -> [
-        e.aggregate_id,
-        e.type
-      ] end)
+
+    #IO.puts Poison.encode!(results)
 
     IO.puts "Found events: #{Enum.count(results)}"
-    IO.puts "Total events: #{Enum.count(events)}"
+    #IO.puts "Total events: #{Enum.count(events)}"
 
   end
+
 end
